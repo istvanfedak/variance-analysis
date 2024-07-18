6007f41d21d78b48a5db22af4ac90d3e65f7e717
Istvan Fedak <istvan.vonfedak@gmail.com>
2024-07-18 02:09:40 -0400

    Updated next config

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/variance-analysis",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
